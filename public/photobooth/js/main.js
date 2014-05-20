f66e9f33d6c4efd17450a7aefa595b823c09d063
Bjorgvin <bjorgvin@meniga.is>
2014-05-20 10:58:23 +0200

    Working on photobooth

--- public/photobooth/js/main.js
+++ public/photobooth/js/main.js
@@ -1,16 +1,56 @@
 $(function(){
-  var username = $('input[data-name="username"]');
-  $(username).keypress(function(event){
-    if (event.charCode == '32') {
+  var $username = $('input[data-name="username"]');
+  var $video = $('#videodivs');
+  var $preview = $('#snapshotdiv');
+  var readyToSubmit = false;
+
+  function getKeyCode(event){
+    return (typeof event.which == "number") ? event.which : event.keyCode;
+  }
+
+  function grabImage(){
+    takeSnapshot();
+    $username.focus();
+    $video.hide();
+    readyToSubmit = true;
+  }
+
+  function discardImage(){
+    $video.show();
+    $preview.hide();
+    $username.val('');
+    readyToSubmit = false;
+  }
+
+  function submitImage(){
+    console.log('Sending image to server...');
+    // TODO: Do some stuff...
+
+    // Wait for the response, display the result for a while, then discard...
+    discardImage();
+  }
+
+  $username.keydown(function(event){
+    var keyCode = getKeyCode(event);
+    if (keyCode == '32') {
+      // Disable space in username input
       event.preventDefault();
       return;
+    } else if (keyCode == '13') {
+      // Submit image
+      submitImage();
     }
   });
-  $(document).keypress(function(event) {
-    if (event.charCode == '32') {
-      console.log('Taking a picture')
-      takeSnapshot();
-      username.focus();
+
+  $(document).keydown(function(event) {
+    var keyCode = getKeyCode(event);
+    if (keyCode == '32') {
+      // Take a picture
+      grabImage();
+    }
+    else if (keyCode == '27') {
+      // Cancel
+      discardImage();
     }
   });
 });
