2da93992c6d1cb9929e89b235ff4d268b048d555
Hlynur Tryggvason (hrparty) <h@x0r.is>
2014-05-25 23:35:09 +0000

    Minor refactor of index.js
    Moved functions out of DOM ready function, removed console.logs and
    made the one left safe for browsers that don’t have window.console.
    Minor code readability changes.

--- public/js/index.js
+++ public/js/index.js
@@ -9,43 +9,6 @@ $(function () {
   sizeHeader();
   fetchImages();
 
-  function fetchImages(){
-    console.log('Fetching images...');
-    $.when($.ajax({
-      method: 'get',
-      dataType: 'json',
-      url: api + 'getfeed',
-      data: { count: 240, lastId: lastId },
-      //success: success
-    })).then(function(data){
-      if (data && data.length > 0){
-        renderImages(data);
-        lastId = data[0].id;
-      }
-    }, function(error){
-      console.log(error);
-    }).done(function(){
-      // Schedule next poll
-      setTimeout(fetchImages, 10000);
-    });
-  }
-
-  function renderImages(data){
-    console.log('Fetched images: ', data);
-    for (var i in data){
-      var $row = $photogrid.find('div.row:first');
-      var imageDiv = '<div class="col-sm-2"><img src="' + imageFolder + data[i].image_filename + '" alt="" /></div>';
-      if (lastId == 0){
-        // First render
-        $row.append(imageDiv);
-      } else {
-        // Incremental render
-        $row.prepend(imageDiv);
-      }
-    }
-    console.log('Done with images');
-  }
-
   var s = skrollr.init({
     constants: {
       logooffset: function() {
@@ -68,4 +31,41 @@ function sizeHeader() {
       viewportheight = $(window).height();
 
   $container.css('height', viewportheight).css('padding-top', (viewportheight - $logo.height()) / 2);
+}
+
+function fetchImages() {
+  $.when(
+    $.ajax({
+      method: 'get',
+      dataType: 'json',
+      url: api + 'getfeed',
+      data: { count: 240, lastId: lastId },
+    })
+  ).then(function(data) {
+    if (data && data.length > 0) {
+      renderImages(data);
+      lastId = data[0].id;
+    }
+  }, function(error){
+    if (typeof(window.console) !== "undefined") {
+      console.log(error);
+    }
+  }).done(function() {
+    // Schedule next poll
+    setTimeout(fetchImages, 10000);
+  });
+}
+
+function renderImages(data) {
+  for (var i in data){
+    var $row = $photogrid.find('div.row:first');
+    var imageDiv = '<div class="col-sm-2"><img src="' + imageFolder + data[i].image_filename + '" alt="" /></div>';
+    if (lastId == 0){
+      // First render
+      $row.append(imageDiv);
+    } else {
+      // Incremental render
+      $row.prepend(imageDiv);
+    }
+  }
 }
