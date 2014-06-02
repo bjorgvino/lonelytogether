589f9c405ad044e4ce975d74aee58ef6db6bfde7
Bjorgvin <bjorgvin@meniga.is>
2014-06-02 18:10:18 +0200

    Changed database connection strategy. Now a connection is opened only when needed, and closed when querying is done. This should eliminate "OperationalError 2006, 'MySQL server has gone away'" error.
    Opening connections is fairly cheap according to http://stackoverflow.com/questions/14883346/how-should-i-establish-and-manage-database-connections-in-a-multi-module-python

--- lonelyapp.py
+++ lonelyapp.py
@@ -3,7 +3,6 @@ from instagram import client, subscriptions
 
 api = None
 reactor = None
-conn = None
 
 def get_config(filename):
   with open(filename) as json_file:
@@ -11,11 +10,6 @@ def get_config(filename):
 
 def get_database_connection():
   print "Connecting to database"
-  global conn
-  if not conn is None and conn.open:
-    print "Returned existing connection object"
-    return conn
-  print "Opening new connection"
   return MySQLdb.connect(host=DATABASE_CONFIG['host'], user=DATABASE_CONFIG['username'], passwd=DATABASE_CONFIG['password'], db=DATABASE_CONFIG['db'])
 
 def get_reactor():
@@ -45,30 +39,33 @@ def process_request(x_hub_signature, raw_response):
     print "Signature mismatch"
 
 def process_tag_update(result):
-  #conn = get_database_connection()
-  global conn
-
   # Fetch images with tag 
   payload = {'count': 5, 'client_id': CONFIG['client_id']}
   r = requests.get('https://api.instagram.com/v1/tags/' + result['object_id'] + '/media/recent', params=payload)
   if r.status_code == 200:
     data = r.json()
-
-    for entry in data['data']:
-      if entry['type'] == 'image':
-        try:
-          cur = conn.cursor()
-          image_url = entry['images']['standard_resolution']['url']
-          entry_id = entry['id']
-          username = entry['user']['username']
-          cur.execute("""INSERT INTO posts (id, username, image_url) VALUES (%s, %s, %s)""", (entry_id, username, image_url))
-          cur.close()
-          conn.commit()
-          print "Saved tag update to database"
-        except Exception, e:
-          print "Error processing tag update"
-          print str(e)
-          conn.rollback()
+    try:
+      conn = get_database_connection()
+      for entry in data['data']:
+        if entry['type'] == 'image':
+          try:
+            cur = conn.cursor()
+            image_url = entry['images']['standard_resolution']['url']
+            entry_id = entry['id']
+            username = entry['user']['username']
+            cur.execute("""INSERT INTO posts (id, username, image_url) VALUES (%s, %s, %s)""", (entry_id, username, image_url))
+            cur.close()
+            conn.commit()
+            print "Saved tag update to database"
+          except Exception, e:
+            print "Error processing tag update"
+            print str(e)
+            conn.rollback()
+    except Exception, e:
+      print "Error processing tag update"
+      print str(e)
+    finally:      
+      conn.close()
   else:
     print "Error when getting data from API"
 
@@ -78,19 +75,23 @@ def process_tag_update(result):
   # print max_tag_id
 
 def get_random_photobooth_entry():
-  global conn
-
-  cur = conn.cursor(MySQLdb.cursors.DictCursor)
-  cur.execute("""SELECT r1.id, r1.username, r1.image_filename FROM photobooth_posts AS r1 JOIN (SELECT (RAND() * (SELECT MAX(id) FROM photobooth_posts)) AS id) AS r2 WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 1;""")
-  data = cur.fetchone()
-  cur.close()
+  try:
+    conn = get_database_connection()
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("""SELECT r1.id, r1.username, r1.image_filename FROM photobooth_posts AS r1 JOIN (SELECT (RAND() * (SELECT MAX(id) FROM photobooth_posts)) AS id) AS r2 WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 1;""")
+    data = cur.fetchone()
+    cur.close()
+  except Exception, e:
+    print "Error getting random photobooth entry"
+    print str(e)
+  finally:
+    conn.close()
   return data
 
 def save_photobooth_entry(username, imageFilename, randomEntryId=0):
-  global conn
-
   print "Saving photobooth entry"
   try:
+    conn = get_database_connection()
     cur = conn.cursor()
     cur.execute("""INSERT INTO photobooth_posts (username, image_filename, paired_id) VALUES (%s, %s, %s)""", (username, imageFilename, randomEntryId))
     insert_id = cur.lastrowid
@@ -98,15 +99,16 @@ def save_photobooth_entry(username, imageFilename, randomEntryId=0):
     conn.commit()
     return insert_id
   except Exception, e:
-      print "Error saving photobooth entry"
-      print str(e)
+    print "Error saving photobooth entry"
+    print str(e)
+  finally:
+    conn.close()
   return 0
 
 def save_lonely_feed_entry(username, username2, image_filename, source, source_id):
-  global conn
-
   print "Saving lonely feed entry"
   try:
+    conn = get_database_connection()
     cur = conn.cursor()
     cur.execute("""INSERT INTO lonely_feed (left_username, right_username, image_filename, source, source_id) VALUES (%s, %s, %s, %s, %s)""", (username, username2, image_filename, source, source_id))
     insert_id = cur.lastrowid
@@ -114,15 +116,17 @@ def save_lonely_feed_entry(username, username2, image_filename, source, source_i
     conn.commit()
     return insert_id
   except Exception, e:
-      print "Error saving lonely feed entry"
-      print str(e)
+    print "Error saving lonely feed entry"
+    print str(e)
+  finally:
+    conn.close()
   return 0
 
 def get_feed(count, lastId):
-  global conn
   try:
     print lastId
     par = (int(lastId), int(count))
+    conn = get_database_connection()
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("SELECT id, left_username, right_username, image_filename FROM lonely_feed WHERE id > %s ORDER BY id DESC LIMIT %s", par)
     data = cur.fetchall()
@@ -130,10 +134,12 @@ def get_feed(count, lastId):
     return json.dumps(data, encoding="iso-8859-1")
   except Exception, e:
     return str(e)
+  finally:
+    conn.close()
 
 def get_entry(entryId):
-  global conn
   try:
+    conn = get_database_connection()
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("SELECT id, left_username, right_username, image_filename FROM lonely_feed WHERE id = %s", (int(entryId),))
     data = cur.fetchall()
@@ -141,6 +147,8 @@ def get_entry(entryId):
     return data
   except Exception, e:
     return str(e)
+  finally:
+    conn.close()
 
 print "Reading configuration files"
 CONFIG = get_config('config/api.json')
@@ -149,7 +157,6 @@ DATABASE_CONFIG = get_config('config/db.json')
 
 api = get_api()
 reactor = get_reactor()
-conn = get_database_connection()
 
 print "Registering subscription callback"
 reactor.register_callback(subscriptions.SubscriptionType.TAG, process_tag_update)
