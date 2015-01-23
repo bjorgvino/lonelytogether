bc50ac16d4a7df9b0cab2d7fc8e6dc0a1601ef2f
Björgvin <bjorgvino@gmail.com>
2015-01-23 10:03:58 +0100

    Fixed database creation and added fixtures.

--- lonelydatabase.py
+++ lonelydatabase.py
@@ -1,14 +1,20 @@
+import datetime
 from sqlalchemy import Column, Index, BigInteger, String, TIMESTAMP
+from sqlalchemy.dialects import mysql, sqlite
 from sqlalchemy.sql.expression import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
- 
+
+BigIntegerType = BigInteger()
+BigIntegerType = BigIntegerType.with_variant(mysql.BIGINT(), 'mysql')
+BigIntegerType = BigIntegerType.with_variant(sqlite.INTEGER(), 'sqlite')
+
 Base = declarative_base()
  
 class PhotoboothEntry(Base):
   __tablename__ = 'photobooth_posts'
   __table_args__ = {'mysql_engine':'InnoDB'}
-  id = Column(BigInteger, primary_key=True)
+  id = Column(BigIntegerType, primary_key=True, autoincrement=True)
   username = Column(String(30), index=True, nullable=False)
   image_filename = Column(String(2000), nullable=False)
   paired_id = Column(BigInteger, nullable=False, server_default="0")
@@ -18,7 +24,7 @@ class PhotoboothEntry(Base):
 class InstagramEntry(Base):
   __tablename__ = 'instagram_posts'
   __table_args__ = {'mysql_engine':'InnoDB'}
-  id = Column(BigInteger, primary_key=True)
+  id = Column(BigIntegerType, primary_key=True, autoincrement=True)
   username = Column(String(30), index=True, nullable=False)
   image_filename = Column(String(2000), nullable=True)
   post_id = Column(String(64), unique=True, nullable=False)
@@ -30,13 +36,14 @@ class InstagramEntry(Base):
 class FeedEntry(Base):
   __tablename__ = 'lonely_feed'
   __table_args__ = {'mysql_engine':'InnoDB'}
-  id = Column(BigInteger, primary_key=True)
+  id = Column(BigIntegerType, primary_key=True, autoincrement=True)
   left_username = Column(String(30), nullable=False)
   right_username = Column(String(30), nullable=False)
   image_filename = Column(String(2000), nullable=False)
   source = Column(String(20), nullable=False)
   source_id = Column(String(64), nullable=False)
-  created_at = Column(TIMESTAMP, server_default=func.now())
+  #created_at = Column(TIMESTAMP, server_default=func.now())
+  created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
 
 Index('left_right_source', FeedEntry.left_username, FeedEntry.right_username, FeedEntry.source_id)
 engine = create_engine('sqlite:///lonelytogether.db')
---
+++ lonelydatabase_fixtures.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from lonelydatabase import Base, FeedEntry
+
+connectionstring = 'sqlite:///lonelytogether.db'
+engine = create_engine(connectionstring)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+entry = FeedEntry(left_username="left1", right_username="right1", image_filename="image1.jpg", source="test", source_id="1")
+session.add(entry)
+print vars(entry)
+session.commit()
